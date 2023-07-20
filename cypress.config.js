9b3ff6acaf9c61709f3691ef783c093f790ebdad
sagarb <sagarb>
2023-07-20 13:14:35 +0530

    new git commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,16 @@
 const { defineConfig } = require("cypress");
-
+const cucumber = require('cypress-cucumber-preprocessor').default
+module.exports = (on, config) => {
+  on('file:preprocessor', cucumber())
+}
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl:"https://parabank.parasoft.com",
-    specPattern:"cypress\\integration\\tests\\*.js",
+   // specPattern:"**/*.feature",
+    specPattern:"cypress/integration/tests/test.js",
     chromeWebSecurity:false
   },
 });
